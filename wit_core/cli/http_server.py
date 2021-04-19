5912c57f533a7cd21dca0359997d4b10b19bafb7
Lucas Oliveira <ls.oliveiras.santos@gmail.com>
2021-04-18 21:08:48 -0300

    feat(cli): added port option for server.

--- wit_core/cli/http_server.py
+++ wit_core/cli/http_server.py
@@ -3,9 +3,10 @@ import uvicorn
 
 
 @click.command()
-def http_server():
+@click.option('--port', default=8000, help='HTTP server port')
+def http_server(port):
     uvicorn.run(
         "wit_core.api.http_server:app",
-        host="0.0.0.0", port=8000,
+        host="0.0.0.0", port=port,
         reload=True
     )
--- wit_core/cli/websocket_server.py
+++ wit_core/cli/websocket_server.py
@@ -3,9 +3,10 @@ import uvicorn
 
 
 @click.command()
-def websocket_server():
+@click.option('--port', default=8000, help='Websocket server port')
+def websocket_server(port):
     uvicorn.run(
         "wit_core.api.websocket_server:app",
-        host="0.0.0.0", port=8080,
+        host="0.0.0.0", port=port,
         reload=True
     )
